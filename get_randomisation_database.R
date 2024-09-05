0d69423629044321e65f098b0a592c6fb4c90ffb
psamonwong <phrutsamon.wongnak@gmail.com>
2024-09-05 08:22:44 +0700

    update analysis

--- get_randomisation_database.R
+++ get_randomisation_database.R
@@ -1,16 +1,16 @@
-data.TH1 <- read.csv("C:/Users/Phrutsamon/Dropbox/PLATCOV/data-TH1.csv")
+data.TH1 <- read.csv("~/Dropbox/PLATCOV/data-TH1.csv")
 data.TH1$Date = as.POSIXct(data.TH1$Date,format='%a %b %d %H:%M:%S %Y')
 data.TH1 = data.TH1[data.TH1$Date <= '2024-04-22',  ]
 
-data.LA8 <- read.csv("C:/Users/Phrutsamon/Dropbox/PLATCOV/data-LA08.csv")
+data.LA8 <- read.csv("~/Dropbox/PLATCOV/data-LA08.csv")
 data.LA8$Date = as.POSIXct(data.LA8$Date,format='%a %b %d %H:%M:%S %Y')
 data.LA8 =  data.LA8[data.LA8$Date <= '2024-04-22',  ]
 
-data.BR3 <- read.csv("C:/Users/Phrutsamon/Dropbox/PLATCOV/data-BR3.csv")
+data.BR3 <- read.csv("~/Dropbox/PLATCOV/data-BR3.csv")
 data.BR3$Date = as.POSIXct(data.BR3$Date,format='%a %b %d %H:%M:%S %Y')
 data.BR3 = data.BR3[data.BR3$Date <= '2024-04-22',  ]
 
-data.PK1 <- read.csv("C:/Users/Phrutsamon/Dropbox/PLATCOV/data-PK01.csv")
+data.PK1 <- read.csv("~/Dropbox/PLATCOV/data-PK01.csv")
 data.PK1$Date = as.POSIXct(data.PK1$Date,format='%a %b %d %H:%M:%S %Y')
 data.PK1 =  data.PK1[data.PK1$Date <= '2024-04-22',  ]
 
@@ -24,6 +24,10 @@ xx = rbind(data.TH1[, c('ID', 'Treatment')],
            data.BR3[, c('ID', 'Treatment')],
            data.PK1[, c('ID', 'Treatment')]
            )
+
+xx <- xx %>% filter(!Treatment %in% c('Nirmatrelvir + Ritonavir + Molnupiravir',
+                               'Hydroxychloroquine',
+                               'Nitazoxanide'))
 
 library(stringr)
 for(i in 1:nrow(xx)){
